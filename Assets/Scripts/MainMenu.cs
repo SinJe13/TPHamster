4ea9761446dd5e76db1de98ec5241e3fa98b7436
SinJe <gaetan.margry17@gmail.com>
2026-03-25 13:44:16 +0100

    MainMenu End

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void OnClickPlay()
     {
-       
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
     }
 }
